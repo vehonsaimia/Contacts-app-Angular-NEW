1067c81fea0f20b38cc114b6388be2b678b0194f
vehonsaimia <vesa.hongisto@student.saimia.fi>
2017-05-31 11:50:56 +0300

    New version of contact app. Login and card parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {MaterialModule, MdIconModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {ContactsComponent} from "./contacts/contacts.component";
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+
+
+
+const routes =
+  [
+    {path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'},
+        {path: 'login',
+      component: LoginComponent},
+    {path: 'contacts',
+      component: ContactsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ContactsComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     FormsModule,
-    HttpModule
+    MdIconModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
